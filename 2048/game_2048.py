047b21a9d2ebb74ecf9ce4db27a403d87e13734e
MJ <maciejjankowski47@gmail.com>
2023-09-19 17:16:43 +0200

    Adds empty gaming loop and instructions for player

--- 2048/game_2048.py
+++ 2048/game_2048.py
@@ -10,6 +10,23 @@ class Game2048():
         """TODO: Write start_game() docstring"""
         print("Welcome to 2048 console game.")
         print("Made by Maciej Jankowski")
+        print("Press ''Enter'' to start the game!")
+        print("Enter 'q' and press 'Enter' to exit.")
+        print("Press arrows and press 'Enter' to play.")
+        print()
+
+        key_input = input()
+
+        #Setting up the playing field
+        self.create_playing_field()
+
+        #Gamplay loop
+        while key_input != 'q':
+            key_input = input()
+
+            match key_input:
+                case 'q':
+                    print("Thanks for playing!")
 
     def create_playing_field(self):
         """TODO: Write create_playing_field() docstring"""
@@ -20,5 +37,6 @@ class Game2048():
         return playing_board
 
 if __name__ == "__main__":
-    print("Hello World!")
+    game = Game2048()
+    game.start_game_loop()
     
